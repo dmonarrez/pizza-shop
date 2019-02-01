e9fc0ec5ed28bee5fca0eae56cb12d40ece1500e
David Monarrez <davidmonarrez3@gmail.com>
2019-02-01 10:46:06 -0800

    add select boxes and checkboxes to get size and toppings input

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,34 @@
+//pizza constructor
 function Pizza () {
-  this.size = 'small';
+  this.size;
   this.toppings = [];
-  this.price = 0;
+  this.toppingsPrice = 0;
+  this.totalPrice = 0;
+};
+//calculating pice of toppings added
+Pizza.prototype.calcToppingPrice = function () {
+  let total = 0;
+  this.toppings.forEach(function (topping) {
+    total += .50;
+  });
+  this.toppingsPrice = total;
 };
 
-Pizza.prototype.getSize = function () {
-
-};
+var pizza = new Pizza();
 
 $(document).ready(function () {
   $('.pizza-form').submit(function (event) {
     event.preventDefault();
-    
+    //get size input and update pizza object
+    let size = $('#size').val();
+    pizza.size = size;
+    //get toppings input and update pizza object
+    $('input:checkbox[name=topping]:checked').each(function (){
+      let toppings = $(this).val();
+      pizza.toppings.push(toppings);
+    });
+    pizza.calcToppingPrice();
+
+    console.log(pizza);
   });
 });
